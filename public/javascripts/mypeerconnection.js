9f546478b9b84b4579a8acf8bc9b7265f76604aa
藪崎卓巳 <raru@syrup16g.net>
2017-10-22 02:03:24 +0900

    change refactor

---
+++ public/javascripts/mypeerconnection.js
@@ -0,0 +1,195 @@
+var raru = raru || {};
+raru.SocketIO = raru.SocketIO || {};
+
+raru.SocketIO.MyRTCPeerConnection = (function() {
+
+    /**
+     * PeerConnectionのコンストラクタ
+     * @param Object socket nodeのwebsocketのインスタンス
+     * @param Array servers turn, stunサーバの設定
+     */
+    var MyRTCPeerConnection = function(socket, servers) {
+        var _localStream;
+        var self = this;
+
+        if(!(this instanceof raru.SocketIO.MyRTCPeerConnection)) {
+            return new raru.SocketIO.MyRTCPeerConnection(socket, servers);
+        }
+        this.servers = servers || { 'iceServers': [{
+            'urls':'stun:stun.l.google.com:19302'
+        }]};
+
+        this.socket = socket;
+        this.ownPeerConnection = new RTCPeerConnection(servers);
+
+        var proto = MyRTCPeerConnection.prototype;
+
+        /**
+         * onicecandidateイベントの設定を行います
+         * @param Function callback
+         */
+        proto.setOnIcecandidate = function (callback) {
+            self.ownPeerConnection.onicecandidate = callback;
+        }
+
+        proto.setOnNegotiationneeded = function (callback) {
+            self.ownPeerConnection.onicecandidate = callback;
+        }
+
+        proto.setOnAddStream = function (callback) {
+            self.ownPeerConnection.onaddstream = callback;
+        }
+
+        proto.setLocalStream = function (stream) {
+            _localStream = stream;
+            self.ownPeerConnection.addStream(_localStream);
+        }
+
+        proto.getLocalStream = function () {
+            return _localStream;
+        }
+
+        proto.setSocketEvent = function (name, callback) {
+            self.socket.on(name, callback);
+        }
+
+        //---------- private functions ----------//
+
+        //---------- web rtc functions ---------//
+
+        /**
+         * 基本的なイベントが設定されたRTCPeerConnectionを返します
+         * @param Object config Object stun/turnサーバの設定
+         * @return RTCPeerConnection 新規コネクション
+         */
+        proto._init = function () {
+            self._initWebRTCEvents();
+            self._initSocketEvents();
+        }
+
+        proto._initWebRTCEvents = function () {
+            // candidate取得時処理
+            self.ownPeerConnection.onicecandidate = function (evt) {
+                if (evt.candidate) {
+                    self._emit('sendCandidate', evt.candidate);
+                }
+            }
+
+            // ネゴシエーションが必要なときに自動でofferを送信
+            self.ownPeerConnection.onnegotiationneeded = function (evt) {
+                self._trace('called on negotiation needed: ' + evt);
+                self._createOffer();
+            }
+        }
+
+        proto._initSocketEvents = function () {
+            /**
+             * offerを受け取り、ansterを返す
+             */
+            self.socket.on('sendOffer', function(offer) {
+                if(!!self.ownPeerConnection) {
+                    self._trace('received offer: ' + offer);
+                    self.ownPeerConnection.setRemoteDescription(new RTCSessionDescription(offer))
+                        .then(self._createAnswer)
+                        .catch(self._defaultErrorHandler('Received Offer'));
+                }
+            });
+
+            self.socket.on('sendCandidate', function (candidate) {
+                self._trace('receive icecandidate: ' + candidate);
+                self.ownPeerConnection.addIceCandidate(new RTCIceCandidate(candidate), self._defaultSuccessHandler, self._defaultErrorHandler('Add Ice Candidate'));
+            });
+
+            self.socket.on('sendAnswer', function (answer) {
+                if(!!self.ownPeerConnection) {
+                    self._trace('received answer: ' + answer);
+                    self.ownPeerConnection.setRemoteDescription(new RTCSessionDescription(answer), self._defaultSuccessHandler, self._defaultErrorHandler('Received Answer'));
+                }
+            });
+        }
+
+        /**
+         * オファーを作成して送信します。
+         */
+        proto._createOffer = function () {
+            self._trace('called create Offer');
+            if(!!self.ownPeerConnection) {
+                self.ownPeerConnection.createOffer()
+                    .then(function (offer) {
+                        self._trace('create offer: ' + offer);
+                        return self.ownPeerConnection.setLocalDescription(new RTCSessionDescription(offer));
+                    })
+                    .then(function () {
+                        self._emit('sendOffer', self.ownPeerConnection.localDescription);
+                    })
+                    .catch(self._defaultErrorHandler('create Offer'));
+            }
+        }
+
+        /**
+         * アンサーを作成して返信します。
+         */
+        proto._createAnswer = function (){
+            self._trace('called create Answer');
+            if (!!self.ownPeerConnection) {
+                self.ownPeerConnection.createAnswer()
+                    .then(function (answer) {
+                        self._trace('create answer' + answer);
+                        return self.ownPeerConnection.setLocalDescription(new RTCSessionDescription(answer));
+                    })
+                    .then(function () {
+                        self._emit('sendAnswer', self.ownPeerConnection.localDescription);
+                    });
+            }
+        }
+
+        //---------- utils ----------//
+        /**
+         * socketを利用してデータ送信を行う。
+         * @param String name イベント名
+         * @param Json param 送信するパラメータ
+         */
+        proto._emit = function (name, param) {
+            self._trace(name + ': ' + param);
+            self.socket.emit(name, param);
+        }
+
+        /**
+         * デフォルトのエラー制御
+         * とりあえずエラーメッセージを表示
+         */
+        proto._defaultErrorHandler = function (context) {
+            return function(error) {
+                self._trace('Failure in ' + context + ': ' + error.toString);
+            }
+        }
+
+        proto._defaultSuccessHandler = function (context) {
+            return function() {
+                self._trace(context + ' is success');
+            }
+        }
+
+        /**
+         * ログ出力用メソッド
+         * @param Object arg ログ出力対象
+         */
+        proto._trace = function (arg) {
+            console.log(self._getCurrentTime() + ': ', arg);
+        }
+
+        /**
+         * 現在日時を取得
+         * @return Date 現在日時
+         */
+        proto._getCurrentTime = function () {
+            return (window.performance.now() / 1000).toFixed(3);
+        }
+
+        this._init();
+    }
+
+
+    return MyRTCPeerConnection;
+})();
+
--- public/javascripts/webrtc.js
+++ public/javascripts/webrtc.js
@@ -1,152 +1,37 @@
 $(function(){
     var socket = io.connect("ws://"+ location.host);
-    var servers = { 'iceServers': [{
-        'urls':'stun:stun.l.google.com:19302'
-    }]};
-    var mediaOption,
-        ownPeerConnection,
-        localStream,
-        displayArea = document.getElementById('display_video');
-
-    /**
-     * システムに必要な初期化処理を行います。
-     */
-    function _init() {
-        mediaOption = {
-            video: true,
-            audio: true
-        }
-        ownPeerConnection = prepareNewPeerConnection(servers);
-        displayArea = document.getElementById('display_video');
-
-        socket.emit('join', {
-            roomName: 'test',
-            name: 'test' + Date.now()
-        });
-    }
+    var ownPeerConnection = new raru.SocketIO.MyRTCPeerConnection(socket);
+    init();
 
     $('#media_auth').on('click', function() {
-        navigator.mediaDevices.getUserMedia(mediaOption)
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        })
             .then(function (stream) { // success
                 localStream = stream;
-                ownPeerConnection.addStream(localStream);
-                trace('success add stream');
+                ownPeerConnection.setLocalStream(localStream);
             }).catch(function (error) { // error
                 console.error('mediaDevice.getUserMedia() error:', error);
                 return;
             });
     });
 
-    /***** 受け取り処理 *****/
-    /**
-     * 通信相手からのofferを受け取りAnswerを返す
-     */
-    socket.on('sendOffer', function(offer) {
-        if(!!ownPeerConnection) {
-            trace('received offer: ' + offer);
-            ownPeerConnection.setRemoteDescription(new RTCSessionDescription(offer), function(){}, errorHandler('Received Offer'));
-            createAnswer();
-        }
-    });
-
-    socket.on('sendCandidate', function (candidate) {
-        trace('receive icecandidate: ' + candidate);
-        ownPeerConnection.addIceCandidate(new RTCIceCandidate(candidate), function(){}, errorHandler('Add Ice Candidate'));
-    });
-
-    socket.on('sendAnswer', function (answer) {
-        if(!!ownPeerConnection) {
-            trace('received answer: ' + answer);
-            ownPeerConnection.setRemoteDescription(new RTCSessionDescription(answer), function(){}, errorHandler('Received Answer'));
-        }
-    });
-
-    // ----- 後々切り出し webRTC
-    /**
-     * 基本的なイベントが設定されたRTCPeerConnectionを返します
-     * @config {Object} stun/turnサーバの設定
-     * @return {RTCPeerConnection} 新規コネクション
-     */
-    function prepareNewPeerConnection(config) {
-        var preparedConnection = new RTCPeerConnection(config);
-
-        // candidate取得時処理
-        preparedConnection.onicecandidate = function (evt) {
-            if (evt.candidate) {
-                trace('send candidate: ' + evt.candidate);
-                socket.emit('sendCandidate', evt.candidate)
-            }
-        }
-
-        // ネゴシエーションが必要なときに自動でofferを送信
-        preparedConnection.onnegotiationneeded = function (evt) {
-            createOffer();
-        }
-
-        preparedConnection.onaddstream = function (evt) {
-            playVideo(document.getElementById('display_video'), evt.stream);
-        }
-
-        return preparedConnection;
-    }
-
     /***** ロジック系関数 *****/
-    function playVideo(element, stream) {
-        trace('play video');
+    function playVideo(evt) {
+        var element = document.getElementById('display_video')
         if ('srcObject' in element) {
-            trace('src object');
-            element.srcObject = stream;
+            element.srcObject = evt.stream;
         } else {
-            trace('src');
-            element.src = window.URL.createObjectURL(stream);
-        }
-        element.play();
-    }
-
-    function createOffer() {
-        trace('called create Offer');
-        if(!!ownPeerConnection) {
-            ownPeerConnection.createOffer()
-            .then(function (offer) {
-                trace('create offer: ' + offer);
-                return ownPeerConnection.setLocalDescription(new RTCSessionDescription(offer));
-            })
-            .then(function () {
-                trace('send offer' + ownPeerConnection.localDescription);
-                socket.emit('sendOffer', ownPeerConnection.localDescription);
-            });
+            element.src = window.URL.createObjectURL(evt.stream);
         }
     }
 
-    function createAnswer() {
-        trace('called create Answer');
-        if (!!ownPeerConnection) {
-            ownPeerConnection.createAnswer()
-            .then(function (answer) {
-                trace('create answer' + answer);
-                return ownPeerConnection.setLocalDescription(new RTCSessionDescription(answer));
-            })
-            .then(function () {
-                trace('send answer');
-                socket.emit('sendAnswer', ownPeerConnection.localDescription);
-            });
-        }
-    }
-
-    /**
-     * 簡易エラーコールバック用メソッド
-     */
-    function errorHandler(context) {
-        return function(error) {
-            trace('Failure in ' + context + ': ' + error.toString);
-        }
-    }
-
-    /**
-     * ログ出力用メソッド
-     */
-    function trace(arg) {
-        var now = (window.performance.now() / 1000).toFixed(3);
-        console.log(now + ': ', arg);
+    function init() {
+        ownPeerConnection.setOnAddStream(playVideo);
+        socket.emit('join', {
+            roomName: 'test',
+            name: 'test' + Date.now()
+        });
     }
 });
