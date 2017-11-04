839b3251d0ce6a5e77be0a9540a51e35fb1b5ee5
藪崎卓巳 <raru@syrup16g.net>
2017-11-04 18:26:21 +0900

    change Common js

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,31 +1,31 @@
 var raru = raru || {};
-raru.Common = {
+raru.Common = (function() {
+    var sockets = [];
 
+    return {
+        isInitialized: function (target) {
+            return typeof target !== "undefined";
+        },
 
-    isInitialized: function (target) {
-        return typeof target !== "undefined";
-    },
-
-    getSocket: function () {
-        if(raru.Common.isInitialized(io) &&
-           raru.Common.isInitialized(io.connect)) {
-            var prefix = 'ws';
-            if ('https:' == document.location.protocol) {
-                prefix = 'wss';
+        getSocket: function () {
+            if(raru.Common.isInitialized(io) &&
+               raru.Common.isInitialized(io.connect)) {
+                var prefix = 'ws';
+                if ('https:' == document.location.protocol) {
+                    prefix = 'wss';
+                }
+                var url = prefix + '://' + location.host;
+                if(!sockets[url]) {
+                    sockets[url] = io.connect(url);
+                }
+                return sockets[url];
             }
-            var url = prefix + '://' + location.host;
-            if(!raru.Common.sockets[url]) {
-                raru.Common.sockets[url] = io.connect(url);
-            }
-            return raru.Common.sockets[url];
+            return null;
+        },
+
+        splitByLine: function (str) {
+            return str.split(/\r\n|\r|\n/);
         }
-        return null;
-    },
+    }
 
-    splitByLine: function (str) {
-        return str.split(/\r\n|\r|\n/);
-    },
-
-    sockets: []
-};
-
+})();
